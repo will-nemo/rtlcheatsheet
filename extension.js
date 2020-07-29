00b1f4478bba69e102b810c5ae724293d18b7c3d
Will <will.lambert96@gmail.com>
2020-07-28 20:16:45 -0400

    Set up initial extension

--- extension.js
+++ extension.js
@@ -1,34 +1,35 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const webViewContent = require('./webViewContent');
+const path = require('path');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rtlcheatsheet" is now active!');
+	const disposableCommand = vscode.commands.registerCommand('rtlcheatsheet.rtlCheatsheet', () => {
+		const styleRoot = vscode.Uri.file(path.join(context.extensionPath, 'style'));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rtlcheatsheet.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+		const panel = vscode.window.createWebviewPanel(
+			'rtlCheatsheet',
+			'React Testing Library Cheatsheet',
+			vscode.ViewColumn.Beside, {
+				localResourceRoots: [
+					styleRoot,
+				]
+			}
+		);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from RTLCheatSheet!');
+		const cspSource = panel.webview.cspSource;
+		const stylePath = panel.webview.asWebviewUri(styleRoot);
+
+		panel.webview.html = webViewContent.getWebViewContent(cspSource, stylePath);
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableCommand);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
